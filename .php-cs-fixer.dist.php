1bf2f4c4fa488e01f286b588191f3aad8b2fcb8d
Florent Devynck <138789832+florentdevk@users.noreply.github.com>
2026-06-28 22:17:09 +0200

    chore: add CI pipeline with PHPStan, PHPUnit and CS Fixer (#1)

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,22 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('vendor')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'declare_strict_types' => true,
+        'strict_param' => true,
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+;
